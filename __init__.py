cf7942a640f1ef478d42058633af9d55a121902d
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-07-13 18:28:19 +0400

    Change frame_combiner to combiner

--- __init__.py
+++ __init__.py
@@ -6,5 +6,5 @@ from .baseline import *
 class BestModel(WindowBasedModel):
     def __init__(self):
         frame_model = FrameModel()
-        frame_combiner_model = FrameCombinerModel()
-        super().__init__(frame_model, frame_combiner_model)
+        combiner_model = CombinerModel()
+        super().__init__(frame_model, combiner_model)
