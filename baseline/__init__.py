3d4b7c75e16711cceaf6e2412f5cc942031ffdc0
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2016-07-13 18:04:40 +0400

    Move baseline to model file

--- baseline/__init__.py
+++ baseline/__init__.py
@@ -1,15 +1,1 @@
-"""Simplest Model."""
-from .. import model
-import numpy as np
-
-
-class Simple(model.Model):
-    def __init__(self):
-        self.output_length = None
-
-    def train(self, x_train, y_train):
-        """Save output length."""
-        self.output_length = len(y_train[0])
-
-    def predict(self, x):
-        return np.ones((len(x), self.output_length))
+from .model import *
